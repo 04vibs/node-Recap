42bc15b05f2b4b60b90bb70306eeabd23308346d
04vibs <04vibs@gmail.com>
2019-04-24 15:35:43 +0530

    modules

---
+++ Lecture-1/globalObject.js
@@ -0,0 +1,15 @@
+// console.log(); //global
+
+// // global objects
+// setTimeout()
+// clearTimeout();
+
+// setInterval();
+// clearInterval()
+
+
+// variable and functions added here are not
+// global as they are only scoped to this file
+var message = '';
+// global.setTimeout()
+console.log(global.message);  // undefined
---
+++ Lecture-1/modules/app.js
@@ -0,0 +1,11 @@
+// var sayHello = function () {
+
+// }
+
+// window.sayHello();
+
+const log = require('./Logger');
+
+log('message logged successfully')
+
+console.log(module);
