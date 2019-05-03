7835fe344d4ca0a82e45ffce2fb0221f4508020d
04vibs <04vibs@gmail.com>
2019-05-03 15:53:59 +0530

    environment

--- Lecture-2-express/Index.js
+++ Lecture-2-express/Index.js
@@ -6,14 +6,20 @@ const authentication = require('./Authentication');
 const express = require('express');
 const app = express();
 
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+// console.log(`app: ${app.get('env')}`);
+
 app.use(express.urlencoded({extended: true})); // key = value & key=value
 app.use(express.static('public'));
 app.use(express.json());
 app.use(logger)
 app.use(authentication);
 app.use(helmet());
-app.use(morgan('tiny'));
 
+if(app.get('env')==='development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...')
+}
 const courses = [
    {id:1,name: 'course1'},
    {id:2,name: 'course2'},
