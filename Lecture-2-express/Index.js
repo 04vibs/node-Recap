0af785db4b9c9903d3f5a81b4e55c25abc131c1e
04vibs <04vibs@gmail.com>
2019-05-03 15:07:01 +0530

    api requests

--- Lecture-2-express/Index.js
+++ Lecture-2-express/Index.js
@@ -19,16 +19,14 @@ app.get('/api/courses',(req,res)=>{
 
 app.post('/api/courses',(req,res)=>{
    
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    const result = Joi.validate(req.body,schema);
-    console.log(result);
 
-    if(!req.body.name || req.body.name.length <3){
+    const {error} = validateCourse(req.body); //destructuring
+    //validate
+    // Id invalid,return 400 - bad request
+    if(error){
         // 400 bad request
-        res.status(400).send(result.error.details[0].message);
-        return;
+       return res.status(400).send(error.details[0].message);
+        
     }
    
     const course = {
@@ -48,6 +46,54 @@ app.get('/api/courses/:id',(req,res)=>{
 app.get('/api/posts/:year/:month',(req,res)=>{
     res.send(req.query);
 });
+
+
+app.put('/api/courses/:id',(req,res)=>{
+    // Look up the course
+    //If not existing, return 404
+    const course = courses.find(c=>c.id === parseInt(req.params.id));
+    if(!course)return res.status(404).send('The course with the given id is not found')
+    
+    // const result = validateCourse(req.body);
+    const {error} = validateCourse(req.body); //destructuring
+    //validate
+    // Id invalid,return 400 - bad request
+    if(error){
+        // 400 bad request
+        return res.status(400).send(error.details[0].message);
+        
+    }
+    //update course
+    course.name = req.body.name;
+
+    //Return the updated course
+    res.send(course);
+})
+
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course,schema);
+   
+}
+
+app.delete('/api/courses/:id',(req,res)=>{
+   // Look up the course 
+   // Not existing, return 404
+   const course = courses.find(c=>c.id === parseInt(req.params.id));
+   if(!course) return res.status(404).send('The course with the given id is not found')
+   
+   //Delete
+   const index = courses.indexOf(course);
+   courses.splice(index,1);
+
+   //Return the same course
+   res.send(course);
+})
+
+
 //Port
 const port = process.env.PORT||3000;
 app.listen(port,()=>{
