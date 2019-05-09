bf5cc3bb9f24275a507793b11a2a1658ec5d5f59
04vibs <04vibs@gmail.com>
2019-05-09 13:22:04 +0530

    mongodb-connected

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,35 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
 .then(()=>console.log('connected to mongodb...'))
 .catch(()=>console.log('could not connect to MongoDb...',err));
+
+// Schema
+const courseSchema = new mongoose.Schema({
+    name:String,
+    author:String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
+
+
+// Classes, objects
+// Course, nodeCourse
+//classes Pascal case
+const Course = mongoose.model('Course',courseSchema);
+// objects camelCase Notation
+
+async function createCourse(){
+   
+    const course = new Course({
+        name:'Angular Course',
+        author: 'Puri',
+        tags: ['angular','frontend'],
+        isPublished: true
+    });
+    
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
+
